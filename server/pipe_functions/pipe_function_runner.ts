d35e52f123edbc36d18eb8c2b6403f68baaf1211
sachin-frayne <sachin.frayne@gmail.com>
2025-03-11 16:25:47 +0100

    clean up space edge case in sed command

--- server/pipe_functions/pipe_function_runner.ts
+++ server/pipe_functions/pipe_function_runner.ts
@@ -1,15 +1,24 @@
 import { sort } from './sort';
 import { sed } from './sed';
+import { badRequest } from '@hapi/boom';
 
 export function processKibanaPipes(
   kibanaPipeCommandURLDecoded: string,
   modifiedResponse: string,
-  catHeader: boolean
+  catHeader: boolean,
+  catCommand: boolean
 ) {
   if (kibanaPipeCommandURLDecoded) {
     const commands = kibanaPipeCommandURLDecoded.split('|');
     for (const commandString of commands) {
-      const [command, ...args] = commandString.trim().split(' ');
+      const [command, ...args] = commandString.match(/(?:[^\s"']+|["'][^"']*["'])+/g) || [];
+      for (const arg of args) {
+        if (catCommand && arg.includes(' ')) {
+          throw badRequest(
+            `Invalid argument: Space character is not allowed in ${command} command arguments for /_cat requests {${arg}}`
+          ).output.payload;
+        }
+      }
       switch (command) {
         case 'sort':
           modifiedResponse = sort(modifiedResponse, args, catHeader);
