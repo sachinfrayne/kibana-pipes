5cd8084769b051895b1001e4b0b2976cd8a1e7eb
sachin-frayne <sachin.frayne@gmail.com>
2025-03-18 12:11:17 +0100

    remove the use of let

--- server/pipe_functions/sed.ts
+++ server/pipe_functions/sed.ts
@@ -1,15 +1,13 @@
 export function sed(input: string, args: string[]): string {
-  let result = input;
-  for (let i = 0; i < args.length; i++) {
-    if (args[i] === '-e' && i + 1 < args.length) {
+  return args.reduce((result, arg, i) => {
+    if (arg === '-e' && i + 1 < args.length) {
       const match = args[i + 1].match(/s\/(.*?)\/(.*?)\/(g?)/);
       if (match) {
         const [_, pattern, replacement] = match;
         const regex = new RegExp(pattern, match[3] === 'g' ? 'g' : '');
-        result = result.replace(regex, replacement);
+        return result.replace(regex, replacement);
       }
-      i++;
     }
-  }
-  return result;
+    return result;
+  }, input);
 }
