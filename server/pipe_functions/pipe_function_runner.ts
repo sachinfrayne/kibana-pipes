5fe301c570348daa078557b5d4deadee26d9b6fc
sachin-frayne <sachin.frayne@gmail.com>
2025-03-26 10:57:47 +0100

    better command split, and some error handling

--- server/pipe_functions/pipe_function_runner.ts
+++ server/pipe_functions/pipe_function_runner.ts
@@ -4,6 +4,26 @@ import { jq } from './jq';
 import { badRequest } from '@hapi/boom';
 import shellQuote from 'shell-quote';
 
+const acceptedCommands = ['sed', 'jq', 'sort'];
+
+export function splitKibanaPipeCommands(kibanaPipeCommand: string): string[][] {
+  const parsedCommand = shellQuote
+    .parse(kibanaPipeCommand)
+    .filter((item): item is string => typeof item === 'string');
+
+  return parsedCommand.reduce<string[][]>(
+    (result, item) => {
+      if (acceptedCommands.includes(item)) {
+        result.push([item]);
+      } else {
+        result[result.length - 1].push(item);
+      }
+      return result;
+    },
+    acceptedCommands.includes(parsedCommand[0]) ? [] : [[]]
+  );
+}
+
 export function processKibanaPipes(
   kibanaPipeCommand: string,
   input: string,
@@ -11,12 +31,14 @@ export function processKibanaPipes(
   catCommand: boolean
 ) {
   if (kibanaPipeCommand) {
-    const commands = kibanaPipeCommand.split('|');
+    const commands = splitKibanaPipeCommands(kibanaPipeCommand);
     for (const commandString of commands) {
-      const parsedCommand = shellQuote
-        .parse(commandString)
-        .filter((item): item is string => typeof item === 'string');
-      const [command, ...args] = parsedCommand;
+      const [command, ...args] = commandString;
+
+      if (!catCommand && command == 'sort') {
+        throw badRequest('Invalid command: sort command is only allowed for /_cat requests').output
+          .payload;
+      }
 
       for (const arg of args) {
         if (catCommand && command == 'sed' && arg.includes(' ')) {
@@ -33,7 +55,7 @@ export function processKibanaPipes(
           input = sed(input, args);
           break;
         case 'jq':
-          input = jq(input, args);
+          input = jq(input, args[0]);
           break;
         default:
           break;
--- tests/pipe_functions/pipe_function_runner.test.ts
+++ tests/pipe_functions/pipe_function_runner.test.ts
@@ -1,7 +1,33 @@
-import { processKibanaPipes } from '../../server/pipe_functions/pipe_function_runner';
+import {
+  splitKibanaPipeCommands,
+  processKibanaPipes,
+} from '../../server/pipe_functions/pipe_function_runner';
 import { test, expect } from '@jest/globals';
 
-test('reverse sort ignoring casing split via processKibanaPipes', () => {
+test('split kibana pipe command with no pipes (sort -r -f)', () => {
+  const processedKibanaPipes = splitKibanaPipeCommands('sort -r -f');
+  expect(processedKibanaPipes).toEqual([['sort', '-r', '-f']]);
+});
+
+test("split kibana pipe command with pipes (sort -r -f | sed -e 's/STRING/SUBSTITUTION/g')", () => {
+  const processedKibanaPipes = splitKibanaPipeCommands(
+    "sort -r -f | sed -e 's/STRING/SUBSTITUTION/g'"
+  );
+  expect(processedKibanaPipes).toEqual([
+    ['sort', '-r', '-f'],
+    ['sed', '-e', 's/STRING/SUBSTITUTION/g'],
+  ]);
+});
+
+test("split kibana pipe command with pipes (sort -r -f | jq '.[0] | {index: .index}'", () => {
+  const processedKibanaPipes = splitKibanaPipeCommands("sort -r -f | jq '.[0] | {index: .index}'");
+  expect(processedKibanaPipes).toEqual([
+    ['sort', '-r', '-f'],
+    ['jq', '.[0] | {index: .index}'],
+  ]);
+});
+
+test('single pipe reverse sort ignoring casing split via processKibanaPipes', () => {
   const processedKibanaPipes = processKibanaPipes(
     'sort -r -f',
     `PbPgpFDVQ6WTRSfi6cMHDQ index1
@@ -16,24 +42,9 @@ PbPgpFDVQ6WTRSfi6cMHDQ index1
 `);
 });
 
-test('sed to change index to i via processKibanaPipes', () => {
-  const processedKibanaPipes = processKibanaPipes(
-    "sed -e 's/index/i/g'",
-    `PbPgpFDVQ6WTRSfi6cMHDQ index1
-pwFuf4UtQp6EXi4_6Jt92w index2
-`,
-    false,
-    true
-  );
-
-  expect(processedKibanaPipes).toEqual(`PbPgpFDVQ6WTRSfi6cMHDQ i1
-pwFuf4UtQp6EXi4_6Jt92w i2
-`);
-});
-
 test('reverse sort and then sed index to i via processKibanaPipes', () => {
   const processedKibanaPipes = processKibanaPipes(
-    "sort -r|sed -e 's/index/i/g'",
+    "sort -r | sed -e 's/index/i/g'",
     `PbPgpFDVQ6WTRSfi6cMHDQ index1
 pwFuf4UtQp6EXi4_6Jt92w index2
 `,
@@ -59,7 +70,50 @@ test('sed with a space in the arg sed -e "s/STRING/SUBSTITUTION STRING/" should 
   } catch (error) {
     expect(error.statusCode).toBe(400);
     expect(error.message).toBe(
-      "Invalid argument: Space character is not allowed in sed command arguments for /_cat requests {s/STRING/SUBSTITUTION STRING/}"
+      'Invalid argument: Space character is not allowed in sed command arguments for /_cat requests {s/STRING/SUBSTITUTION STRING/}'
     );
   }
+});
+
+test('sort on anything but a cat command should throw an error', () => {
+  try {
+    processKibanaPipes(
+      'sort -r',
+      `{
+         "index": "index1"
+       }
+  `,
+      false,
+      false
+    );
+  } catch (error) {
+    expect(error.statusCode).toBe(400);
+    expect(error.message).toBe(
+      'Invalid command: sort command is only allowed for /_cat requests'
+    );
+  }
+});
+
+test('sed then jq with a pipe in the arg must parse through correctly via processKibanaPipes', () => {
+  const processedKibanaPipes = processKibanaPipes(
+    "sed -e 's/index/awesomeindex/' | jq '.[0] | {index: .awesomeindex}'",
+    `[
+  {
+    "health": "green",
+    "index": "index1"
+  },
+  {
+    "health": "green",
+    "index": "index2"
+  }
+]
+`,
+    false,
+    true
+  );
+
+  expect(processedKibanaPipes).toEqual(`{
+  "index": "index1"
+}
+`);
 });
