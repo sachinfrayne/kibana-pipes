e3a6ff3e87bf62ae53734ea41090c42381fc0ac9
sachin-frayne <sachin.frayne@gmail.com>
2025-03-26 10:57:20 +0100

    adds a restrict feature to jq

--- server/pipe_functions/jq.ts
+++ server/pipe_functions/jq.ts
@@ -1,14 +1,25 @@
-export function jq(input: string, args: string[]): string {
+export function jq(input: string, arg: string): string {
   const json = JSON.parse(input);
-  for (let i = 0; i < args.length; i++) {
-    const match = args[i].match(/\.\[.*?]/);
-    if (match) {
-      const arrayNum = match[0].match(/^\.\[(\d+)]/);
-      if (arrayNum !== null) {
-        let newVar = arrayNum[1];
-        return JSON.stringify(json[newVar], null, 2);
-      }
-    }
-  }
-  return '{}';
+
+  const [arrayAccess, operation] = arg
+    .replace(/'/g, '')
+    .split('|')
+    .map((part) => part.trim());
+
+  const arrayMatch = arrayAccess.match(/\.\[(\d+)]/);
+  const intermediateResult = arrayMatch ? json[parseInt(arrayMatch[1], 10)] : json;
+
+  const finalResult =
+    operation && operation.startsWith('{') && operation.endsWith('}')
+      ? operation
+          .slice(1, -1)
+          .split(',')
+          .reduce((acc, field) => {
+            const [key, value] = field.split(':').map((part) => part.trim());
+            acc[key] = intermediateResult[value.slice(1)];
+            return acc;
+          }, {} as Record<string, unknown>)
+      : intermediateResult;
+
+  return JSON.stringify(finalResult, null, 2) + '\n';
 }
--- tests/pipe_functions/jq.test.ts
+++ tests/pipe_functions/jq.test.ts
@@ -16,9 +16,53 @@ test('jq first object in JSON array _cat/indices?h=index,health,status&format=js
 ]
 `;
 
-  expect(jq(input, ["'.[0]'"])).toEqual(`{
+  expect(jq(input, "'.[0]'")).toEqual(`{
   "index": "index1",
   "health": "green",
   "status": "open"
-}`);
+}
+`);
+});
+
+test('jq index from first object in JSON array _cat/indices?h=index,health,status&format=json', () => {
+  const input = `[
+  {
+    "index": "index1",
+    "health": "green",
+    "status": "open"
+  },
+  {
+    "index": "index2",
+    "health": "green",
+    "status": "open"
+  }
+]
+`;
+
+  expect(jq(input, "'.[0] | {index: .index}'")).toEqual(`{
+  "index": "index1"
+}
+`);
+});
+
+test('jq index and health from first object in JSON array _cat/indices?h=index,health,status&format=json', () => {
+  const input = `[
+  {
+    "index": "index1",
+    "health": "green",
+    "status": "open"
+  },
+  {
+    "index": "index2",
+    "health": "green",
+    "status": "open"
+  }
+]
+`;
+
+  expect(jq(input, "'.[0] | {index: .index, health: .health}'")).toEqual(`{
+  "index": "index1",
+  "health": "green"
+}
+`);
 });
